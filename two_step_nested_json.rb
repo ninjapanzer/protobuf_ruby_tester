0c4c156a60d2ce628fc109c18717225935641c00
Paul Scarrone <pscarrone@taskrabbit.com>
2023-07-20 13:56:55 -0400

    Create two_step_nested_json.rb

---
+++ two_step_nested_json.rb
@@ -0,0 +1,29 @@
+## Loads the Protos
+Dir[File.join(__dir__, './vendor/proto/protos', '**.rb')].each do |file|
+  name = File.basename(file, '.rb').gsub!(/_pb$/, '').split('_').map(&:capitalize).join
+  require file
+end
+
+nested = File.read("json/nested_json.json")
+
+puts "The Source JSON"
+puts nested
+
+## Capture the whole json 
+message = Google::Protobuf.decode_json(
+  Tester::Proto::NestedJSON,
+  nested,
+  ignore_unknown_fields: true,
+)
+
+puts message.data
+
+nested_message = message.data.map do |d|
+  Google::Protobuf.decode_json(
+    Tester::Proto::Data,
+    d.to_json,
+    ignore_unknown_fields: true,
+    )
+end
+
+puts nested_message.to_json
